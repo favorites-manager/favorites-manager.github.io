a1d83ed5c6bac580c2a79fe063eee92708eebeec
--global <--global>
2023-10-12 22:06:55 +0300

    csv -> json

--- scrape_html.py
+++ scrape_html.py
@@ -1,4 +1,4 @@
-import sys, re, csv, shutil, os, webbrowser
+import sys, re, json, shutil, os, webbrowser
 
 # Assign values from input arguments
 HTML = str(sys.argv[1])	# The HTML file of the TikTok liked videos page
@@ -9,11 +9,10 @@ CURRENT_LIKED_MEDIA_FN = str(sys.argv[3])	# CSV of media you've already liked, p
 NEW_THUMBNAILS_DIR = 'new_liked'
 
 # Create an array of all the media you've already liked
-already_liked = []
+already_liked_ids = []
 with open(CURRENT_LIKED_MEDIA_FN, 'r') as f:
-	reader = csv.reader(f)
-	for row in reader:
-		already_liked.append(row[1])
+	already_liked = json.load(f)
+	already_liked_ids = [media['id'] for media in already_liked]
 
 # Extract the HTML file into a single string
 lines = ''
@@ -25,17 +24,24 @@ os.makedirs(NEW_THUMBNAILS_DIR, exist_ok=True)
 # Get every liked video and its corresponding thumbnail from the saved HTML file
 regex = r'<a href="(https://www.tiktok.com/@.+?/video/\d+?)".+?src="[^/]+?/([^/]+?\.jpeg)".+?href="\1"'
 firstFlag = True
-with open('new_liked.csv', 'w') as f:
-	writer = csv.writer(f)
-	for m in re.finditer(regex, lines):
-		MEDIA_URL = m.group(1)	# e.g. https://www.tiktok.com/@favorites-manager/video/3141592653589790288
-		MEDIA_THUMBNAIL = m.group(2)	# e.g. o07feIsQnhmOhEKlgDbpA7AkF8RTyIGIlSejf8~tplv-photomode-zoomc.jpeg
-		MEDIA_ID = MEDIA_URL.split('/')[-1]	# e.g. 3141592653589790288
+new_liked = []
+for m in re.finditer(regex, lines):
+	MEDIA_URL = m.group(1)	# e.g. https://www.tiktok.com/@favorites-manager/video/3141592653589790288
+	MEDIA_THUMBNAIL = m.group(2)	# e.g. o07feIsQnhmOhEKlgDbpA7AkF8RTyIGIlSejf8~tplv-photomode-zoomc.jpeg
+	MEDIA_ID = MEDIA_URL.split('/')[-1]	# e.g. 3141592653589790288
+	
+	# If you haven't already tagged this piece of media, play it in the browser and tag it
+	if MEDIA_ID not in already_liked_ids:
+		webbrowser.open(MEDIA_URL, 1 if firstFlag else 0)
+		tags = input("Comma-Separated Tags: " if firstFlag else "Tags: ")
+		firstFlag = False
+		new_liked.append({
+			'platform': 'tiktok',
+			'id': MEDIA_ID,
+			'url': MEDIA_URL,
+			'tags': tags.split(',')
+		})
 
-		# If you haven't already tagged this piece of media, play it in the browser and tag it
-		if MEDIA_ID not in already_liked:
-			webbrowser.open(MEDIA_URL, 1 if firstFlag else 0)
-			tags = input("Comma-Separated Tags: " if firstFlag else "Tags: ")
-			firstFlag = False
-			writer.writerow(['tiktok', MEDIA_ID, MEDIA_URL] + tags.split(','))
-			shutil.copyfile(THUMBNAILS+MEDIA_THUMBNAIL, '{}/{}.jpg'.format(NEW_THUMBNAILS_DIR, MEDIA_ID))
+		with open('new_liked.json', 'w') as f:
+			json.dump(new_liked, f, ensure_ascii=False, indent='\t')
+		shutil.copyfile(THUMBNAILS+MEDIA_THUMBNAIL, '{}/{}.jpg'.format(NEW_THUMBNAILS_DIR, MEDIA_ID))
